c2be6d32fb6da5de218c8b0a81b2cad1068503e9
Aditya Sankhla <adityasa@iitbhilai.ac.in>
2023-01-16 01:00:41 +0530

    almost done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
-import 'question.dart';
+import 'questionBank.dart';
+
+QuizBrain q_ans = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -31,7 +34,25 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Icons> progressIcons = [];
+  List<Widget> progressIcons = [];
+
+  void checkUserAnswer(bool userAnswer) {
+    bool correctAns = q_ans.getAnswer();
+
+    if (correctAns == userAnswer) {
+      progressIcons.add(Icon(
+        Icons.check,
+        color: Colors.green,
+        size: 20,
+      ));
+    } else {
+      progressIcons.add(Icon(
+        Icons.close_outlined,
+        color: Colors.red,
+        size: 20,
+      ));
+    }
+  }
 
   // List<String> questionList = [
   //   'You can lead a cow down stairs but not up stairs.',
@@ -40,16 +61,6 @@ class _QuizPageState extends State<QuizPage> {
   // ];
   //
   // List<bool> questionAnswers = [false, true, true];
-
-  List<Question> qna = [
-    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
-    Question(
-        q: 'Approximately one quarter of human bones are in the feet.',
-        a: true),
-    Question(q: 'A slug\'s blood is green.', a: true)
-  ];
-
-  int questionNumber = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -63,7 +74,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                qna[questionNumber].question,
+                q_ans.getQuestion(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -78,15 +89,19 @@ class _QuizPageState extends State<QuizPage> {
             padding: const EdgeInsets.all(15.0),
             child: TextButton(
               onPressed: () {
-                bool correctAns = qna[questionNumber].answer;
-
-                if (correctAns == true) {
-                  print('You have got it right.');
-                } else {
-                  print("You have got it wrong");
-                }
                 setState(() {
-                  questionNumber++;
+                  checkUserAnswer(true);
+                  q_ans.nextQuestion();
+                  if (q_ans.checkEnd()) {
+                    progressIcons.clear();
+                    q_ans.resetQ();
+                    Alert(
+                            context: context,
+                            title: "Quiz End",
+                            desc:
+                                "The quiz has ended.Hope you gained knowledge.")
+                        .show();
+                  }
                 });
               },
               style: TextButton.styleFrom(
@@ -107,16 +122,19 @@ class _QuizPageState extends State<QuizPage> {
             padding: const EdgeInsets.all(15.0),
             child: TextButton(
               onPressed: () {
-                bool correctAns = qna[questionNumber].answer;
-
-                if (correctAns == false) {
-                  print('You have got it right.');
-                } else {
-                  print("You have got it wrong");
-                }
-
                 setState(() {
-                  questionNumber++;
+                  checkUserAnswer(false);
+                  q_ans.nextQuestion();
+                  if (q_ans.checkEnd()) {
+                    progressIcons.clear();
+                    q_ans.resetQ();
+                    Alert(
+                            context: context,
+                            title: "Quiz End",
+                            desc:
+                                "The quiz has ended.Hope you gained knowledge.")
+                        .show();
+                  }
                 });
               },
               style: TextButton.styleFrom(
@@ -133,6 +151,10 @@ class _QuizPageState extends State<QuizPage> {
           ),
         ),
         //TODO: Add a Row here as your score keeper
+
+        Row(
+          children: progressIcons,
+        )
       ],
     );
   }
